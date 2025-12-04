4ae05354bb2aae1167a3a199fa5712ac391520b8
sariasca <santyac27@hotmail.com>
2025-12-04 19:17:14 +0100

    - Preparando el backend

---
+++ backend/src/main/java/com/sariasca/notesapp/NotesappApplication.java
@@ -0,0 +1,13 @@
+package com.sariasca.notesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesappApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(NotesappApplication.class, args);
+	}
+
+}
