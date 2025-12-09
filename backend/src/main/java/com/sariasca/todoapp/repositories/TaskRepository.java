51e2109955015b7bb8442deef1fb09c2b1bd21cb
sariasca <santyac27@hotmail.com>
2025-12-09 17:03:13 +0100

    - Repositorios preparados - Separación de ENUM priority

--- backend/src/main/java/com/sariasca/todoapp/repositories/TaskRepository.java
+++ backend/src/main/java/com/sariasca/todoapp/repositories/TaskRepository.java
@@ -4,9 +4,15 @@ import com.sariasca.todoapp.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 
 public interface TaskRepository extends JpaRepository<Task, Integer> {
-
+    List<Task> findByName(String name);
+    List<Task> findByFinished(Boolean finished);
+    List<Task> findByCategoryId(Integer categoryId);
+    List<Task> findByFinishedAndCategoryId(Boolean finish, Integer categoryId);
+    long countByCategoryId(Integer categoryId);
 
 }
